5ad2d4fe8e38428e8eb18e1238736cf6f3e9446e
yasmeen <yasmeenhany14@gmail.com>
2024-03-06 23:01:17 +0200

    fix lint

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -6,6 +6,6 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
     const user = await createUser({ firstName, lastName });
     return { photo, user };
   } catch (error) {
-    return { photo: null, user: null};
+    return { photo: null, user: null };
   }
 }
