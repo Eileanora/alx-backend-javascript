b833369132a0ad7a80edb52c70c47f1221a762bd
yasmeen <yasmeenhany14@gmail.com>
2024-03-07 22:38:09 +0200

    rem tasks

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,29 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    /* eslint-disable */
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(newSize) {
+    this._size = newSize;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(newLocation) {
+    this._location = newLocation;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') return this._size;
+    if (hint === 'string') return this._location;
+    return null;
+  }
+}
